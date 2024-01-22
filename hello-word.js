9e0aa44ac0e528229651b2d9c0524bb1022b35e0
Fernando <marshmallow34657@gmail.com>
2024-01-22 20:12:41 -0300

    primeiro hello-word

--- hello-word.js
+++ hello-word.js
@@ -1,3 +1,10 @@
 const devTest = "hello-word";
 
 console.log(devTest);
+
+let cidade = "Chapecó";
+let eu = "Fernando"
+let idade = 23;
+let altura = 1.73;
+
+console.log(`Meu nome é ${eu},moro na cidade de ${cidade},tenho ${idade} anos e minha altura é de ${altura}m.`)
