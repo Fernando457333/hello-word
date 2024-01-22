3888ddb258eadfc3983c1d5443211f7341d182c0
Fernando <marshmallow34657@gmail.com>
2024-01-22 20:05:04 -0300

    hello-word

--- hello-word.js
+++ hello-word.js
@@ -1,11 +1,3 @@
 const devTest = "hello-word";
 
 console.log(devTest);
-
-const eu = {
-    nome: "Fernando",
-    idade: 24,
-    altura: 1.74
-}
-
-console.log(`Meu nome é ${nome},tenho ${idade} anos e possuo ${altura}m de altura`)
